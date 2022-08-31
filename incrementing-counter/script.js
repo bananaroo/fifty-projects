a4321eb32b5c757e030209a7d2b7003b1f79e4a4
anna_k <ackongs@gmail.com>
2022-08-30 22:09:16 -0600

    js completed

--- incrementing-counter/script.js
+++ incrementing-counter/script.js
@@ -5,7 +5,16 @@ counters.forEach(counter => {
 
     const updateCounter = () => {
         const target = +counter.getAttribute('data-target')
-        console.log(typeof target, target)
+        const c = +counter.innerText
+
+        const increment = target / 300
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
+        }
     }
 
     updateCounter()
