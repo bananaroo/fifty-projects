290d482c75179939ccd3169ee556410808c15a49
anna_k <ackongs@gmail.com>
2022-09-02 22:44:59 -0600

    js formed

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,1 +1,30 @@
-const API_URL = ('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d1f0eae22b1248c0bc351a67899a76c4&page=1')
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d1f0eae22b1248c0bc351a67899a76c4&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=d1f0eae22b1248c0bc351a67899a76c4&query="'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+//Get initial movies
+getMovies(API_URL)
+
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    console.log(data.results)
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
